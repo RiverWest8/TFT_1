6977b4941cbd77f8833f9837e674da558b2ae38a
RiverWest8 <50421431+RiverWest8@users.noreply.github.com>
2025-08-25 11:59:22 +0100

    Add files via upload

--- TFT.py
+++ TFT.py
@@ -2866,7 +2866,7 @@ if __name__ == "__main__":
     val_hist_csv = LOCAL_RUN_DIR / f"tft_val_history_e{MAX_EPOCHS}_{RUN_SUFFIX}.csv"
     val_hist_cb  = ValLossHistory(val_hist_csv)
     
-    lr_decay_cb = EpochLRDecay(gamma=0.95, start_epoch=9) 
+    lr_decay_cb = EpochLRDecay(gamma=0.95, start_epoch=8) 
 
     # ----------------------------
     # Trainer instance
