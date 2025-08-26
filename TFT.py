e5ad3cab3f6e323a08cb3f9bb0e8d6bfb407696d
RiverWest8 <50421431+RiverWest8@users.noreply.github.com>
2025-08-27 00:58:03 +0100

    Add files via upload
    Still thinks I have 2 es

--- TFT.py
+++ TFT.py
@@ -2863,7 +2863,7 @@ if __name__ == "__main__":
         gradient_clip_val=GRADIENT_CLIP_VAL,
         num_sanity_val_steps = 0,
         logger=logger,
-        callbacks=[best_ckpt_cb, TQDMProgressBar(refresh_rate=50), es_cb, metrics_cb, mirror_cb, cosine_cb, lr_cb, val_hist_cb] + EXTRA_CALLBACKS,
+        callbacks=[best_ckpt_cb, TQDMProgressBar(refresh_rate=50), es_cb, metrics_cb, mirror_cb, lr_cb, val_hist_cb] + EXTRA_CALLBACKS,
         check_val_every_n_epoch=int(ARGS.check_val_every_n_epoch),
         log_every_n_steps=int(ARGS.log_every_n_steps),
         enable_progress_bar=True,
